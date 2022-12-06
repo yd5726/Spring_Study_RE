62fd6d367c0e4287e08032561089f26dc04e3e35
MINJU KIM <yd5726@naver.com>
2022-12-06 17:51:49 +0900

    Create spring02
    Create spring02

---
+++ spring02/src/test/java/com/hanul/web02/DBConnTest.java
@@ -0,0 +1,12 @@
+package com.hanul.web02;
+
+import java.sql.Connection;
+
+import org.springframework.test.context.ContextConfiguration;
+
+//@ContextConfiguration
+public class DBConnTest {
+	void connect() {
+		Connection conn = null;
+	}
+}
